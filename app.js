e65b25483e6cf7e839c23c73e1120d55556bd1a9
Mariana <marianafelixdelima7@gmail.com>
2023-08-26 21:21:43 -0300

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,5 @@
+const hello = document.querySelector("#heythere");
+const backendDev = document.querySelector("#backenddev");
+
+const helloMotion = hello.value, backMotion = backendDev.value;
+let count

e715d4f3106ebc4c24345c1aa67804b56e87fe77
Mariana Félix <129334365+mari-felix@users.noreply.github.com>
2024-07-16 17:51:05 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const hello = document.querySelector("#heythere");
-const backendDev = document.querySelector("#backenddev");
 
-const helloMotion = hello.value, backMotion = backendDev.value;
-let count
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+
+const app = express();
+
+// Configuração para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public'), {
+    acceptRanges: true, // Permite solicitações de intervalo de bytes
+    etag: false, // Desativa a geração de ETags para simplificar
+    lastModified: false // Desativa o uso de datas de modificação
+  }));
+  
+  // Rota básica para servir o arquivo index.html
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  // Inicia o servidor
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
